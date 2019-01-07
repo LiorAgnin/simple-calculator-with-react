2e3683444f37d7c90193251964247b46d6fd7391
LiorAgnin <lior.agnin@routeperfect.com>
2019-01-07 16:41:47 +0200

    Updates

---
+++ precache-manifest.988b7d3980299760c5b88ffda24711da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "299d97e780e938aeeadf",
+    "url": "/simple-calculator-with-react/static/js/runtime~main.299d97e7.js"
+  },
+  {
+    "revision": "b86bec98e6706c08c062",
+    "url": "/simple-calculator-with-react/static/js/main.b86bec98.chunk.js"
+  },
+  {
+    "revision": "c6ad30e7d34f9cc4f9e2",
+    "url": "/simple-calculator-with-react/static/js/1.c6ad30e7.chunk.js"
+  },
+  {
+    "revision": "b86bec98e6706c08c062",
+    "url": "/simple-calculator-with-react/static/css/main.a3e1160d.chunk.css"
+  },
+  {
+    "revision": "f3fd58552943352839981bb5f327585a",
+    "url": "/simple-calculator-with-react/index.html"
+  }
+];
